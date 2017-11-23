a95b993f2d545b7bba1efab8954703376f036879
DiViNiZe <patric_emmel@outlook.com>
2017-11-23 10:27:46 +0700

    Edit modal

--- components/Head.jsx
+++ components/Head.jsx
@@ -5,60 +5,72 @@ import { Row, Col, Card, Button, Input, Icon } from 'react-materialize'
 import Modal from 'react-modal'
 
 
-const logo = require("../asset/img/logo.png")
 
-
-const hstyle = {
-    backgroundColor: '#00695c',
-    color: 'Black',
-    height: '170px',
+class Head extends React.Component{
+constructor(props){
+    super(props)
+    this.state={isOpen:false}
 }
 
-const Heading = styled.div`
-    padding:30px 0px 0px 180px;
-    width:40px;
-    position: absolute;
-    font-size:33px;
-    font-style:italic;
-`
-const Space = styled.div`
-height:5px;
-`
-
-const setting = {
-    color: '#eceff1',
-    opacity: '0.8',
-}
-
-const Set = styled.div`
-    text-align : right;
-    margin-right : 10px;
-    margin-top : 5px;
-`
-const Head = () => (
-    <div style={hstyle} className='z-depth-2'>
-        <Space />
-        <ShopLogo img={logo} />
-        <div>
-            <Heading>
-                DASHBORD
-                </Heading>
-        </div>
-        <Modal isOpen = {true}>
-            <Set>
-                <a style={setting} className="waves-effect" href="#modal1"><Icon>settings</Icon></a>
-            </Set>
-            <div id="modal1" className="modal modal-fixed-footer">
-                <div className="modal-content">
-                    <h4>Modal Header</h4>
-                    <p>A bunch of text</p>
+render(){
+    
+    const logo = require("../asset/img/logo.png")
+    
+    
+    const hstyle = {
+        backgroundColor: '#00695c',
+        color: 'Black',
+        height: '170px',
+    }
+    
+    const Heading = styled.div`
+        padding:30px 0px 0px 180px;
+        width:40px;
+        position: absolute;
+        font-size:33px;
+        font-style:italic;
+    `
+    const Space = styled.div`
+    height:5px;
+    `
+    
+    const setting = {
+        color: '#eceff1',
+        opacity: '0.8',
+    }
+    
+    const Set = styled.div`
+        margin-right : 10px;
+        margin-top : 5px;
+        float:right;
+    `
+    var greeting = 'hey'
+    const openModal = ()=>{
+        this.setState = {isOpen:true}
+    }
+    
+    const closeModal = ()=>{
+        this.setState = {isOpen:false}
+    }
+        return(
+            
+            <div>
+                <Space />
+                <Set>
+                        <a style={setting} className="waves-effect" onClick={openModal}><Icon>settings</Icon></a>
+                </Set>
+                <ShopLogo style={{clear:'both'}} img={logo} />
+                <div>
+                    <Heading>
+                        DASHBORD
+                    </Heading>
                 </div>
-                <div className="modal-footer">
-                    <a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat ">Agree</a>
-                </div>
+                <Modal isOpen = {this.state.isOpen}>
+
+                </Modal>
             </div>
-        </Modal>
-    </div>
-)
+        )
+    }
+}
 
 export default Head
